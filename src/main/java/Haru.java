fe4ede5cffd9f97191a22efc1fd0699c7f2e19ef
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-19 16:32:36 +0800

    Haru.java: Add greet and exit messages

--- src/main/java/Haru.java
+++ src/main/java/Haru.java
@@ -1,4 +1,14 @@
 public class Haru {
+    private static void greet() {
+        System.out.println("Hi~! I'm Haru. What can I do for you today?");
+    }
+
+    private static void exit() {
+        System.out.println("See you next time! Bye-bye~");
+    }
+
     public static void main(String[] args) {
+        greet();
+        exit();
     }
 }
