c4a2a97e8c9a71d58ba5cf055604b26e7851fb55
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-19 18:03:58 +0800

    Haru.java: Rename greet() to hello() and exit() to goodbye()

--- src/main/java/Haru.java
+++ src/main/java/Haru.java
@@ -1,14 +1,14 @@
 public class Haru {
-    private static void greet() {
+    private static void hello() {
         System.out.println("Hi~! I'm Haru. What can I do for you today?");
     }
 
-    private static void exit() {
+    private static void goodbye() {
         System.out.println("See you next time! Bye-bye~");
     }
 
     public static void main(String[] args) {
-        greet();
-        exit();
+        hello();
+        goodbye();
     }
 }
