c2a0d583400c5af28ad0690fe80ae9fe1ebd12bb
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-19 18:43:12 +0800

    Add basic parser skeleton with echo output

--- src/main/java/Goodbye.java
+++ src/main/java/Goodbye.java
@@ -2,5 +2,6 @@ public class Goodbye extends Command {
     @Override
     public void execute() {
         System.out.println("See you next time! Bye-bye~");
+        Haru.stop();
     }
 }
--- src/main/java/Haru.java
+++ src/main/java/Haru.java
@@ -1,6 +1,27 @@
+import java.util.stream.Stream;
+
 public class Haru {
+    private static boolean isRunning = true;
+
+    public static void stop() {
+        Haru.isRunning = false;
+    }
+
+    private static void parse(String str) {
+        System.out.println("You typed: " + str);
+        String[] tokens = Stream.of(str.split(" "))
+                .filter(t -> !t.isEmpty())
+                .toArray(String[]::new);
+        // TODO: tokens can be empty
+        if (tokens[0].equals("bye")) {
+            new Goodbye().execute();
+        }
+    }
+
     public static void main(String[] args) {
         new Hello().execute();
-        new Goodbye().execute();
+        while (Haru.isRunning) {
+            Haru.parse(System.console().readLine());
+        }
     }
 }
