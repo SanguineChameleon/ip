77f4cecda88f06881bf2082f79bf9503c7be40f8
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-21 11:44:11 +0800

    Add constructors to Hello.java and Goodbye.java

--- src/main/java/Goodbye.java
+++ src/main/java/Goodbye.java
@@ -1,4 +1,8 @@
 public class Goodbye extends Command {
+    public Goodbye() {
+        super(new String[]{});
+    }
+
     @Override
     public void execute() {
         System.out.println("See you next time! Bye-bye~");
--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,4 +1,8 @@
 public class Hello extends Command {
+    public Hello() {
+        super(new String[]{});
+    }
+
     @Override
     public void execute() {
         System.out.println("Hi~! I'm Haru. What can I do for you today?");
