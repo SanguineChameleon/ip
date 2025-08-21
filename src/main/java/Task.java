db2a0cda5461adf8a145cefe148d7feb78e0ac7a
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-21 19:36:50 +0800

    Task.java: Add completion status

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,27 @@
 public class Task {
     private final String name;
+    private boolean isDone = false;
 
     public Task(String name) {
         this.name = name;
     }
 
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.name;
+    }
+
     @Override
     public String toString() {
-        return this.name;
+        return String.format("[%c] %s",
+                (this.isDone ? 'X' : ' '),
+                this.getDescription());
     }
 }
