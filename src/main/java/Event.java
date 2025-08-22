52149b8809ca6a5048e38a9436d3a2ccf54b1cf9
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 10:47:57 +0800

    Add Event task class

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+    private final String from;
+    private final String to;
+
+    public Event(String name, String from, String to) {
+        super(name, 'E');
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("%s (from %s to %s)", this.getName(), this.from, this.to);
+    }
+}
