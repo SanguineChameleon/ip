a7fd5d0322880b18ca8dd24b40f59d7ee8d6cd39
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 12:31:51 +0800

    AddToDo.java: refactor and throw exception

--- src/main/java/AddToDo.java
+++ src/main/java/AddToDo.java
@@ -1,12 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddToDo extends AddTask {
     public AddToDo() {
-        super(new String[]{});
+        super(new HashMap<>(Map.of("primary", "task name")));
     }
 
     @Override
-    public void execute() {
-        // TODO: task name can be empty
-        String name = super.getOption("primary");
+    public void execute() throws HaruException {
+        String name = super.getRequiredOption("primary");
         add(new ToDo(name));
     }
 }
