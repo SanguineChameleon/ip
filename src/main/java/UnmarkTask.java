3c227c14b7486b3c412383cf0b7c41c01077ce94
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 12:47:48 +0800

    UnmarkTask.java: refactor and throw exception

--- src/main/java/UnmarkTask.java
+++ src/main/java/UnmarkTask.java
@@ -1,13 +1,14 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class UnmarkTask extends Command {
     public UnmarkTask() {
-        super(new HashMap<>());
+        super(new HashMap<>(Map.of("primary", "task number")));
     }
 
     @Override
-    public void execute() {
-        int id = Haru.parseTaskId(this.getOption("primary"));
+    public void execute() throws HaruException {
+        int id = Haru.parseTaskId(this.getRequiredOption("primary"));
         Task task = Haru.getTasks().get(id);
         task.unmark();
         System.out.println("Okay~! I will unmark this task as not done:");
