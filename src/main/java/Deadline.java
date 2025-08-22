1e6a26cfd92d23740d030c5ec550568fade40c20
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 10:40:06 +0800

    Add Deadline task class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    private final String by;
+
+    public Deadline(String name, String by) {
+        super(name, 'D');
+        this.by = by;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("%s (by: %s)", this.getName(), this.by);
+    }
+}
