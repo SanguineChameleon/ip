ec9ca783db30e3f1d22cd5d0ffce27eafadde252
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 10:15:50 +0800

    AddTask.java: add constructor

--- src/main/java/AddTask.java
+++ src/main/java/AddTask.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 
 public abstract class AddTask extends Command {
+    public AddTask(String[] names) {
+        super(names);
+    }
+
     public void add(Task task) {
         System.out.println("Okay~! I will add this task to your list:");
         System.out.println(task);
