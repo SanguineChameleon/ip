bda2ee83ecd5cf68ce14abdc1c93bb12d43e334b
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 10:13:51 +0800

    Make AddTask class abstract

--- src/main/java/AddTask.java
+++ src/main/java/AddTask.java
@@ -1,15 +1,7 @@
 import java.util.ArrayList;
 
-public class AddTask extends Command {
-    public AddTask() {
-        super(new String[]{});
-    }
-
-    @Override
-    public void execute() {
-        // TODO: task name can be empty
-        String name = super.getOption("primary");
-        Task task = new Task(name);
+public abstract class AddTask extends Command {
+    public void add(Task task) {
         System.out.println("Okay~! I will add this task to your list:");
         System.out.println(task);
         ArrayList<Task> tasks = Haru.getTasks();
