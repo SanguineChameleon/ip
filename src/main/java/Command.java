7cf3f467ab68a98e3700b8160ccc193b4b120226
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 10:29:28 +0800

    Command.java: Implement option parsing

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -15,14 +15,22 @@ public abstract class Command {
 
     public void parse(String[] tokens) {
         StringBuilder sb = new StringBuilder();
+        String name = "primary";
         for (int i = 1; i < tokens.length; i++) {
             String token = tokens[i];
+            if (token.charAt(0) == '/') {
+                this.options.put(name, sb.toString());
+                name = token.substring(1);
+                // TODO: check if name is invalid
+                sb = new StringBuilder();
+                continue;
+            }
             if (!sb.isEmpty()) {
                 sb.append(' ');
             }
             sb.append(token);
         }
-        this.options.put("primary", sb.toString());
+        this.options.put(name, sb.toString());
     }
 
     public abstract void execute();
