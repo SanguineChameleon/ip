4eea8d9b22abdfe0948269edb1ab8919f3236b09
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 12:57:16 +0800

    Add InvalidTaskIdException

---
+++ src/main/java/InvalidTaskIdException.java
@@ -0,0 +1,8 @@
+public class InvalidTaskIdException extends HaruException {
+    public InvalidTaskIdException(int length) {
+        super((length == 0)
+                ? "Eh?! The list is currently empty!"
+                : String.format("Eh?! The task ID must be a number between 1 and %d!", length)
+        );
+    }
+}
