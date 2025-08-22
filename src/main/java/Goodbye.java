2e2a12e77909cac019efb4723d202ec5f3b9482c
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-22 13:16:38 +0800

    Goodbye.java: Add exclamation point at the end

--- src/main/java/Goodbye.java
+++ src/main/java/Goodbye.java
@@ -7,7 +7,7 @@ public class Goodbye extends Command {
 
     @Override
     public void execute() {
-        System.out.println("See you next time! Bye-bye~");
+        System.out.println("See you next time! Bye-bye~!");
         Haru.stop();
     }
 }
