1ffd3bfeb341a3d2ab17cfa1254e08e24f5dd7c8
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-27 14:17:29 +0800

    ListTasks.java: refactor execute() method to use TaskList

--- src/main/java/ListTasks.java
+++ src/main/java/ListTasks.java
@@ -9,9 +9,6 @@ public class ListTasks extends Command {
     @Override
     public void execute() {
         System.out.println("Okay~! Here are all the tasks in your list:");
-        ArrayList<Task> tasks = Haru.getTasks();
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + ". " + tasks.get(i));
-        }
+        System.out.println(this.getTaskList());
     }
 }
