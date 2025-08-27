3bb48367f6c5645f59dff6bfcfeb9fcb55eaf4bc
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-27 09:22:49 +0800

    Add TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class TaskList implements Serializable {
+    private final String filePath;
+    private final ArrayList<Task> tasks;
+
+    private TaskList(String filePath, ArrayList<Task> tasks) {
+        this.filePath = filePath;
+        this.tasks = tasks;
+    }
+
+    public static TaskList empty(String filePath) {
+        return new TaskList(filePath, new ArrayList<>());
+    }
+
+    public static TaskList fromFile(String filePath)
+            throws IOException, ClassNotFoundException {
+        try (FileInputStream fis = new FileInputStream(filePath);
+             ObjectInputStream in = new ObjectInputStream(fis)) {
+            return (TaskList) in.readObject();
+        }
+    }
+
+    public void writeToFile()
+        throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(this.filePath);
+             ObjectOutputStream out = new ObjectOutputStream(fos)) {
+            out.writeObject(this);
+        }
+    }
+}
