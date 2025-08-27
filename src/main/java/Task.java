2415ecfb9f1c553ec6db972cd13f9e847cdc33cb
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-27 14:33:10 +0800

    Task.java: implement Serializable

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,6 @@
-public abstract class Task {
+import java.io.Serializable;
+
+public abstract class Task implements Serializable {
     private final String name;
     private final TaskType type;
     private boolean isDone = false;
