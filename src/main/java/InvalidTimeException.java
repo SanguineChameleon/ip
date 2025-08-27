783efcdb13a95bb32e12f2a1a632adb79056cae1
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-27 16:09:02 +0800

    Add InvalidTimeException class

---
+++ src/main/java/InvalidTimeException.java
@@ -0,0 +1,5 @@
+public class InvalidTimeException extends HaruException {
+    public InvalidTimeException(String alias) {
+        super(String.format("Eh?! The %s must be in a valid format!", alias));
+    }
+}
