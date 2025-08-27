22706264732ab6d1fbf23b00d4014fe1c0acd40d
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-27 15:54:24 +0800

    Add TaskTime class

---
+++ src/main/java/TaskTime.java
@@ -0,0 +1,34 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.Locale;
+
+public class TaskTime {
+    private static final List<DateTimeFormatter> FORMATS = List.of(
+            DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm", Locale.ENGLISH),
+            DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"),
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+    );
+
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.ENGLISH);
+
+    private LocalDateTime time;
+
+    public TaskTime(String alias, String strTime) throws HaruException {
+        for (DateTimeFormatter fmt : FORMATS) {
+            try {
+                this.time = LocalDateTime.parse(strTime, fmt);
+                return;
+            } catch (DateTimeParseException _) {
+            }
+        }
+        // todo: throw exception
+    }
+
+    @Override
+    public String toString() {
+        return this.time.format(OUTPUT_FORMAT);
+    }
+}
