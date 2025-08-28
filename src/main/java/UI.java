afad5cbdb3a5dd4db6ccc8c5abd8bec941c52f16
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-28 15:19:26 +0800

    Add UI class

---
+++ src/main/java/UI.java
@@ -0,0 +1,9 @@
+public class UI {
+    public String readLine() {
+        return System.console().readLine();
+    }
+
+    public void show(String msg) {
+        System.out.println(msg);
+    }
+}
