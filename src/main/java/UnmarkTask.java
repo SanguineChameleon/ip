f206041b639dc575e68a093089a0cd2fafab302f
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-28 15:41:13 +0800

    UnmarkTask.java: use UI for output

--- src/main/java/UnmarkTask.java
+++ src/main/java/UnmarkTask.java
@@ -12,7 +12,7 @@ public class UnmarkTask extends Command {
         TaskList taskList = this.getTaskList();
         int id = taskList.parseTaskId(this.getRequiredOption("primary"));
         Task task = taskList.unmark(id);
-        System.out.println("Okay~! I will unmark this task as not done:");
-        System.out.println(task);
+        this.getUI().show("Okay~! I will unmark this task as not done:");
+        this.getUI().show(task.toString());
     }
 }
