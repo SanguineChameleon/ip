55bc348c60c919ff3297aff11bccc8a05d712230
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-28 15:40:50 +0800

    Goodbye.java: use UI for output

--- src/main/java/Goodbye.java
+++ src/main/java/Goodbye.java
@@ -7,7 +7,7 @@ public class Goodbye extends Command {
 
     @Override
     public void execute() {
-        System.out.println("See you next time! Bye-bye~!");
+        this.getUI().show("See you next time! Bye-bye~!");
         Haru.stop();
     }
 }
