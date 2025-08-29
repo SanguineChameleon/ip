5fdee3efe306c69efa021654dc527af3509f8454
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-29 08:19:21 +0800

    TaskList.java: replace wildcard import with explicit imports

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class TaskList implements Serializable {
