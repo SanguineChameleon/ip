3e681363e30f560a882994e261d7f6de333689e3
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-29 11:14:26 +0800

    Task.java: add JavaDoc comments

--- src/main/java/haru/model/Task.java
+++ src/main/java/haru/model/Task.java
@@ -2,32 +2,58 @@ package haru.model;
 
 import java.io.Serializable;
 
+/**
+ * Base class for all tasks.
+ */
 public abstract class Task implements Serializable {
     private final String name;
     private final TaskType type;
     private boolean isDone = false;
 
+    /**
+     * Constructs a Task with name and type.
+     * @param name the task name
+     * @param type the task type
+     */
     public Task(String name, TaskType type) {
         this.name = name;
         this.type = type;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void mark() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks the task as not done.
+     */
     public void unmark() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the task name.
+     * @return the task name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Returns the task description.
+     * @return the task description
+     */
     public String getDescription() {
         return this.getName();
     }
 
+    /**
+     * Returns the string representation of the task.
+     * @return the string representation
+     */
     @Override
     public String toString() {
         return String.format("[%c][%c] %s",
