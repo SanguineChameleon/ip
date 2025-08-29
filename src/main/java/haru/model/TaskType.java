b1f7beb52a04b36d482779a923455834daa66706
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-29 11:14:41 +0800

    TaskType.java: add JavaDoc comments

--- src/main/java/haru/model/TaskType.java
+++ src/main/java/haru/model/TaskType.java
@@ -1,5 +1,8 @@
 package haru.model;
 
+/**
+ * Represents the type of a task.
+ */
 public enum TaskType {
     TODO('T'),
     DEADLINE('D'),
@@ -7,10 +10,18 @@ public enum TaskType {
 
     private final char code;
 
+    /**
+     * Constructs a TaskType with code.
+     * @param code the task type code
+     */
     TaskType(char code) {
         this.code = code;
     }
 
+    /**
+     * Returns the task type code.
+     * @return the task type code
+     */
     public char getCode() {
         return this.code;
     }
