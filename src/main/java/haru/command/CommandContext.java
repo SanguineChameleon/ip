16c80918e32e97517b5174f8955f5cc022267d97
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-29 11:12:57 +0800

    CommandContext.java: add JavaDoc comments

--- src/main/java/haru/command/CommandContext.java
+++ src/main/java/haru/command/CommandContext.java
@@ -3,19 +3,35 @@ package haru.command;
 import haru.model.TaskList;
 import haru.ui.UI;
 
+/**
+ * Holds context information for command execution.
+ */
 public class CommandContext {
     private final TaskList taskList;
     private final UI ui;
 
+    /**
+     * Constructs a CommandContext with task list and UI.
+     * @param taskList the task list
+     * @param ui the UI
+     */
     public CommandContext(TaskList taskList, UI ui) {
         this.taskList = taskList;
         this.ui = ui;
     }
 
+    /**
+     * Returns the task list.
+     * @return the task list
+     */
     public TaskList getTaskList() {
         return this.taskList;
     }
 
+    /**
+     * Returns the UI.
+     * @return the UI
+     */
     public UI getUI() {
         return this.ui;
     }
