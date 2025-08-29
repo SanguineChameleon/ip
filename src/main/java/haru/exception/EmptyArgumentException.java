af887614fb22b628eabeff1ddf271d266d3cc743
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-29 11:13:16 +0800

    EmptyArgumentException.java: add JavaDoc comments

--- src/main/java/haru/exception/EmptyArgumentException.java
+++ src/main/java/haru/exception/EmptyArgumentException.java
@@ -1,6 +1,14 @@
 package haru.exception;
 
+/**
+ * Exception thrown when a required argument is empty.
+ */
 public class EmptyArgumentException extends HaruException {
+
+    /**
+     * Constructs an EmptyArgumentException with the missing alias.
+     * @param alias the name of the missing argument
+     */
     public EmptyArgumentException(String alias) {
         super(String.format("Eh?! The %s cannot be empty!", alias));
     }
