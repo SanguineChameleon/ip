26c7c570a3a1c08e65f27521bc3b4c335287b198
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-29 11:13:52 +0800

    Hello.java: add JavaDoc comments

--- src/main/java/haru/command/Hello.java
+++ src/main/java/haru/command/Hello.java
@@ -2,11 +2,22 @@ package haru.command;
 
 import java.util.HashMap;
 
+/**
+ * Command to greet the user.
+ */
 public class Hello extends Command {
+
+    /**
+     * Constructs a Hello command.
+     * @param ctx command context for execution
+     */
     public Hello(CommandContext ctx) {
         super(new HashMap<>(), ctx);
     }
 
+    /**
+     * Executes the command to show a greeting message.
+     */
     @Override
     public void execute() {
         this.getUI().show("Hi~! I'm Haru. What can I do for you today?");
