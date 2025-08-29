019c93cc0dad214488226b3f5aefcd7d0a65eb50
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-29 11:13:35 +0800

    Goodbye.java: add JavaDoc comments

--- src/main/java/haru/command/Goodbye.java
+++ src/main/java/haru/command/Goodbye.java
@@ -4,11 +4,22 @@ import haru.app.Haru;
 
 import java.util.HashMap;
 
+/**
+ * Command to exit the application.
+ */
 public class Goodbye extends Command {
+
+    /**
+     * Constructs a Goodbye command.
+     * @param ctx command context for execution
+     */
     public Goodbye(CommandContext ctx) {
         super(new HashMap<>(), ctx);
     }
 
+    /**
+     * Executes the command to stop the application.
+     */
     @Override
     public void execute() {
         this.getUI().show("See you next time! Bye-bye~!");
