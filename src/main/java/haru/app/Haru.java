2b831dcb3df54237817978822e80ed64f8094023
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-30 15:00:22 +0800

    Haru.java: add sample Hello World program

--- src/main/java/haru/app/Haru.java
+++ src/main/java/haru/app/Haru.java
@@ -18,6 +18,8 @@ import haru.exception.EmptyCommandException;
 import haru.exception.HaruException;
 import haru.exception.UnknownCommandException;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -68,7 +70,11 @@ public class Haru extends Application {
 
     @Override
     public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
+        stage.setScene(scene); // Setting the stage to show our scene
+        stage.show(); // Render the stage.
     }
 
 /*    public static void main(String[] args) {
