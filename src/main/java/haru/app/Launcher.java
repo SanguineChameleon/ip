74c262efe64398d09152a126436a38d21b46e66d
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-08-30 14:46:32 +0800

    Add Launcher.java class

---
+++ src/main/java/haru/app/Launcher.java
@@ -0,0 +1,15 @@
+package haru.app;
+
+import javafx.application.Application;
+
+/**
+ * Launcher class to start the Haru JavaFX application.
+ */
+public class Launcher {
+    /**
+     * Entry point that launches the Haru JavaFX application.
+     */
+    public static void main(String[] args) {
+        Application.launch(Haru.class, args);
+    }
+}
