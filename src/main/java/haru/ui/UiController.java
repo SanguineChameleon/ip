d04b6d119b766382947d682bb569b4fbad27e390
SanguineChameleon <97085374+SanguineChameleon@users.noreply.github.com>
2025-09-15 12:20:19 +0800

    UiController.java: use ChatGPT to add Javadoc

--- src/main/java/haru/ui/UiController.java
+++ src/main/java/haru/ui/UiController.java
@@ -1,3 +1,5 @@
+// ChatGPT helped write the Javadoc
+
 package haru.ui;
 
 import javafx.fxml.FXML;
@@ -22,22 +24,47 @@ public class UiController {
     @FXML
     private Button send;
 
+    /**
+     * Returns the scroll pane containing the chat.
+     *
+     * @return the ScrollPane for chat
+     */
     public ScrollPane getScroll() {
         return scroll;
     }
 
+    /**
+     * Returns the chat box container.
+     *
+     * @return the VBox for chat
+     */
     public VBox getChat() {
         return chat;
     }
 
+    /**
+     * Returns the bottom input bar.
+     *
+     * @return the HBox for input and send button
+     */
     public HBox getBottom() {
         return bottom;
     }
 
+    /**
+     * Returns the input text field.
+     *
+     * @return the TextField for user input
+     */
     public TextField getInput() {
         return input;
     }
 
+    /**
+     * Returns the send button.
+     *
+     * @return the Button to send messages
+     */
     public Button getSend() {
         return send;
     }
